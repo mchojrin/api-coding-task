3629a38848e51cdaec55adc1ed42a72a296aeb35
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2024-10-08 14:45:47 +0200

    Added entities definition

---
+++ app/src/Entity/Character.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharacterRepository;
+use DateTimeInterface;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CharacterRepository::class)]
+#[ORM\Table(name: '`characters`')]
+class Character
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 128)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE)]
+    private ?DateTimeInterface $birth_date = null;
+
+    #[ORM\Column(length: 128)]
+    private ?string $kingdom = null;
+
+    #[ORM\ManyToOne(inversedBy: 'characters')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Equipment $equipment = null;
+
+    #[ORM\ManyToOne(inversedBy: 'characters')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Faction $faction = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?DateTimeInterface
+    {
+        return $this->birth_date;
+    }
+
+    public function setBirthDate(DateTimeInterface $birth_date): static
+    {
+        $this->birth_date = $birth_date;
+
+        return $this;
+    }
+
+    public function getKingdom(): ?string
+    {
+        return $this->kingdom;
+    }
+
+    public function setKingdom(string $kingdom): static
+    {
+        $this->kingdom = $kingdom;
+
+        return $this;
+    }
+
+    public function getEquipment(): ?Equipment
+    {
+        return $this->equipment;
+    }
+
+    public function setEquipment(?Equipment $equipment): static
+    {
+        $this->equipment = $equipment;
+
+        return $this;
+    }
+
+    public function getFaction(): ?Faction
+    {
+        return $this->faction;
+    }
+
+    public function setFaction(?Faction $faction): static
+    {
+        $this->faction = $faction;
+
+        return $this;
+    }
+}
