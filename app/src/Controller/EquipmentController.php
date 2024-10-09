ab4df21f238e297dfcca3e53f7e0a5b8a789376f
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2024-10-09 08:54:42 +0200

    Completed Equipment CRUD

--- app/src/Controller/EquipmentController.php
+++ app/src/Controller/EquipmentController.php
@@ -6,17 +6,19 @@ use App\Entity\Equipment;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/equipment')]
 class EquipmentController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $entityManager)
+    public function __construct(private readonly EntityManagerInterface $entityManager)
     {
     }
 
-    #[Route('/equipment', name: 'equipment_list')]
-    #[Cache(public: true, maxage: 3600, mustRevalidate: true)]
+    #[Route('/', name: 'equipment_list', methods: ['GET'])]
+    #[Cache(maxage: 3600, public: true, mustRevalidate: true)]
     public function index(): JsonResponse
     {
         return $this->json(
@@ -27,10 +29,65 @@ class EquipmentController extends AbstractController
         );
     }
 
-    #[Route('/equipment/{id}', name: 'an_equipment')]
-    #[Cache(public: true, maxage: 3600, mustRevalidate: true)]
+    #[Route('/', name: 'equipment_list', methods: ['POST'])]
+    #[Cache(maxage: 3600, public: true, mustRevalidate: true)]
+    public function create(Request $request): JsonResponse
+    {
+        $requestBody = json_decode($request->getContent(), true);
+
+        $newEquipment = new Equipment(
+            name: $requestBody['name'],
+            type: $requestBody['type'],
+            made_by: $requestBody['made_by'],
+        );
+
+        $this->entityManager->persist($newEquipment);
+        $this->entityManager->flush();
+
+        return $this->json(
+            [
+                'id' => $newEquipment->getId(),
+            ]);
+    }
+
+    #[Route('/{id}', name: 'an_equipment', methods: ['GET'])]
+    #[Cache(maxage: 3600, public: true, mustRevalidate: true)]
     public function detail(Equipment $equipment): JsonResponse
     {
         return $this->json($equipment);
+    }
+
+    #[Route('/{id}', name: 'delete_equipment', methods: ['DELETE'])]
+    public function delete(Equipment $toDelete): JsonResponse
+    {
+        $this->entityManager->remove($toDelete);
+        $this->entityManager->flush();
+
+        return $this->json([]);
+    }
+
+    #[Route('/{id}', name: 'update_equipment', methods: ['PATCH'])]
+    public function update(Equipment $toUpdate, Request $request): JsonResponse
+    {
+        $changes = json_decode($request->getContent(), true);
+
+        foreach ($changes as $key => $value) {
+            switch ($key) {
+                case "name":
+                    $toUpdate->setName($value);
+                    break;
+                case "type":
+                    $toUpdate->setType($value);
+                    break;
+                case "made_by":
+                    $toUpdate->setMadeBy($value);
+                    break;
+            }
+        }
+
+        $this->entityManager->persist($toUpdate);
+        $this->entityManager->flush();
+
+        return $this->json([]);
     }
 }
---
+++ app/tests/Controller/EquipmentControllerTest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Entity\Equipment;
+use App\Entity\User;
+use App\Repository\EquipmentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EquipmentControllerTest extends WebTestCase
+{
+    private readonly EquipmentRepository $equipmentRepository;
+    private readonly EntityManagerInterface $entityManager;
+    private string $token;
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        $this->entityManager = self::getContainer()->get('doctrine')->getManager();
+        $this->equipmentRepository = $this->entityManager->getRepository(Equipment::class);
+
+        $this->createUser();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->removeUser();
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAllowCreatingNewEquipments(): void
+    {
+        $newEquipmentData = [
+            'name' => 'New equipment',
+            'type' => 'New equipment type',
+            'made_by' => 'New equipment made by',
+        ];
+
+        $this->client->request(
+            'POST',
+            '/equipment',
+            server: [
+                'HTTP_X-AUTH-TOKEN' => $this->token,
+            ],
+            content: json_encode($newEquipmentData),
+        );
+
+        $this->assertResponseIsSuccessful();
+        $actualResult = json_decode(
+            $this->client->getResponse()->getContent(),
+            true
+        );
+        $newEquipment = $this->equipmentRepository->findOneBy(['id' => $actualResult['id']]);
+        $this->assertEquals($newEquipment->getName(), $newEquipmentData['name']);
+        $this->assertEquals($newEquipment->getType(), $newEquipmentData['type']);
+        $this->assertEquals($newEquipment->getMadeBy(), $newEquipmentData['made_by']);
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function shouldAllowDeletingEquipments(): void
+    {
+        $toDelete = new Equipment(
+            name: 'Delete me',
+            type: 'Equipment to be deleted',
+            made_by: 'Made by to be deleted'
+        );
+
+        $this->entityManager->persist($toDelete);
+        $this->entityManager->flush();
+
+        $this->assertNotEmpty($this->equipmentRepository->find($toDelete->getId()));
+
+        $this->client->request(
+            'DELETE',
+            '/equipment/' . $toDelete->getId(),
+            server: [
+                'HTTP_X-AUTH-TOKEN' => $this->token,
+            ],
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEmpty($this->equipmentRepository->findOneBy(['id' => $toDelete->getId()]));
+    }
+
+    /**
+     * @throws ORMException
+     * @test
+     */
+    public function shouldAllowUpdatingEquipments(): void
+    {
+        $toUpdate = new Equipment(
+            name: 'Change me',
+            type: 'Equipment to be updated type',
+            made_by: 'Equipment to be updated made by',
+        );
+
+        $this->entityManager->persist($toUpdate);
+        $this->entityManager->flush();
+        $this->assertNotEmpty($this->equipmentRepository->find($toUpdate->getId()));
+
+        $this->client->request(
+            'PATCH',
+            '/equipment/' . $toUpdate->getId(),
+            server: [
+                'HTTP_X-AUTH-TOKEN' => $this->token,
+            ],
+            content: json_encode(['name' => "Changed"])
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->entityManager->refresh($toUpdate);
+        $this->assertEquals("Changed", $toUpdate->getName());
+    }
+
+    protected function createUser(): void
+    {
+        $this->token = bin2hex(random_bytes(36));
+        $user = new User();
+        $user->setToken($this->token);
+        $user->setRoles(['ROLE_USER']);
+        $user->setEmail("test@test.com");
+        $user->setPassword("password");
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+
+    protected function removeUser(): void
+    {
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['token' => $this->token]);
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+    }
+}
